fdcf62c405fe32b8c7f155db8eafe56fef2be855
ProCodersEg <codecafe.eg@gmail.com>
2024-05-01 05:16:03 +0300

    sddfgh
    dfdfgh

--- index.js
+++ index.js
@@ -45,9 +45,3 @@ function updateDocuments() {
 cron.schedule('* * * * *', () => {
   updateDocuments();
 });
-
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
