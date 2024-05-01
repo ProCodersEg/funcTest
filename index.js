cba4b9b0d80e34568a61142e902f3212c881a05a
ProCodersEg <66762184+ProCodersEg@users.noreply.github.com>
2024-05-01 04:56:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -45,3 +45,9 @@ function updateDocuments() {
 cron.schedule('* * * * *', () => {
   updateDocuments();
 });
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
